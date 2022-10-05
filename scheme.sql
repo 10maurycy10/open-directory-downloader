f3feb986620691c06733d467ecf0d569bee8af7f
10maurycy10 <10maurycy10@gmail.com>
2022-10-04 17:42:34 -0700

    schema

---
+++ scheme.sql
@@ -0,0 +1,24 @@
+drop table if exists queue;
+drop table if exists hashes;
+drop table if exists paths;
+drop table if exists unsupported;
+
+create table unsupported (full text);
+
+create table queue (
+	url text,
+	hostname varchar(256),
+	starthost varchar(256),
+	retry_count int
+);
+
+create table paths (
+	hostname varchar(256),
+	proto varchar(16),
+	filepath varchar(256),
+	full text,
+	blobid varchar(256)
+);
+
+create index hostname on queue (hostname);
+create index url on queue (url);
